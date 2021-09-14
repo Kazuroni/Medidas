372ef215aa41f7ba455bf4d649b5adaa12d40e08
Kazuroni <sayajinbr30@gmail.com>
2021-09-14 18:16:45 -0300

    Finalização

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Medidas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valormetros;
+            
+            Console.Write("digite um valor em Metro(s): ");
+            valormetros = double.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.ForegroundColor=ConsoleColor.DarkRed;
+            Console.WriteLine("= = = Equivalêcia = = =");
+            Console.ResetColor();
+
+            Console.WriteLine($"{valormetros * 100} cm");
+            Console.WriteLine($"{valormetros} m");
+            Console.WriteLine($"{valormetros / 1000} Km");
+
+            Console.WriteLine();
+            Console.WriteLine("Aperte qualquer tecla para Finalizar...");
+            Console.ReadKey();
+        }
+    }
+}
